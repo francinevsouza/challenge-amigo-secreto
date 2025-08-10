d6ce42e2b2d295de6916e0246e4c65c23964c4c3
Francine Souza <francinesouza025@gmail.com>
2025-08-10 13:13:45 -0300

    projeto inicial

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,45 @@
+// criando lista de amigos vazia
+let listaDeAmigos = [];
+
+//função para adicionar amigos na lista
+function adicionarAmigo() {
+    let nomeAmigo = document.getElementById("amigo").value;
+    if (nomeAmigo == "") {
+        alert("Por favor, insira um nome válido.");
+    } else {
+        listaDeAmigos.push(nomeAmigo);
+        document.getElementById("amigo").value ="";
+        atualizarLista();
+    }
+}
+
+//função para atualizar a lista de amigos
+function atualizarLista() {
+    let listaAtualizada = document.getElementById("listaAmigos");
+    listaAtualizada.innerHTML = "";
+    for (let i = 0; i < listaDeAmigos.length; i++) {
+        let itemLista = document.createElement("li");
+        itemLista.innerHTML = listaDeAmigos[i];
+        listaAtualizada.appendChild(itemLista);
+    }
+}
+
+//função para sortear amigo
+function sortearAmigo() {
+    if (listaDeAmigos.length == 0) {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * listaDeAmigos.length);
+    let nomeSorteado = listaDeAmigos[indiceAleatorio];
+
+    document.getElementById("resultado").innerHTML = `Seu amigo secreto sorteado é: ${nomeSorteado}`;
+}  
+
+//função para reiniciar sorteio
+function reiniciarSorteio() {
+    listaDeAmigos = [];
+    document.getElementById("resultado").innerHTML = "";
+    atualizarLista();
+}
